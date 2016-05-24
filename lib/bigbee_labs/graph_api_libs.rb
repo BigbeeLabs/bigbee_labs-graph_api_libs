1db678c41584570dd59d2c32f149bb113005ab52
Joseph <villajosephe@gmail.com>
2016-05-24 06:41:28 -0700

    added require active_support/dependencies

--- lib/bigbee_labs/graph_api_libs.rb
+++ lib/bigbee_labs/graph_api_libs.rb
@@ -1,6 +1,7 @@
 #require "bigbee_labs/graph_api/base"
 require "bigbee_labs/graph_api_libs/version"
 require 'bigbee_labs/remote_models_base'
+require 'active_support/dependencies'
 
 #Dir[Gem::Specification.find_by_name("bigbee_labs-graph_api_libs").gem_dir + "/lib/bigbee_labs/graph_api/acts_as/**/*.rb"].each{|f| require f}
 Dir[Gem::Specification.find_by_name("bigbee_labs-graph_api_libs").gem_dir + "/lib/bigbee_labs/graph_api/**/*.rb"].each{|f| require f}
