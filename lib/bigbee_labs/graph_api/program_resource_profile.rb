efed32c44fe46c801c7b1150cbc7ff7ac5d50e60
Joseph <villajosephe@gmail.com>
2016-07-12 10:56:09 -0700

    pre-build 0.0.20

---
+++ lib/bigbee_labs/graph_api/program_resource_profile.rb
@@ -0,0 +1,15 @@
+module BigbeeLabs
+  module GraphApi
+    module ProgramResourceProfile
+
+      def self.included(base)
+        base.class_eval do 
+          my_klass.remote_attributes  = [:id, :name, :description, :system_name, :logo_url, :errors]
+          my_klass.remote_permits     = [:id, :name, :description, :system_name, :logo_url, :slug]
+          my_klass.remote_requires    = [:name, :description, :system_name, :logo_url]
+        end
+      end
+
+    end
+  end
+end
--- lib/bigbee_labs/graph_api_libs/version.rb
+++ lib/bigbee_labs/graph_api_libs/version.rb
@@ -1,5 +1,5 @@
 module BigbeeLabs
   module GraphApiLibs
-    VERSION = "0.0.19"
+    VERSION = "0.0.20"
   end
 end
