34ff4b497437c21fd9d2ebfa3184e93d89285bee
joseph-bigbee <joseph@bigbeelabs.com>
2016-12-16 14:12:41 -0800

    adding role.rb

---
+++ lib/bigbee_labs/graph_api/role.rb
@@ -0,0 +1,15 @@
+module BigbeeLabs
+  module GraphApi
+
+    module Role
+      def self.included(base)
+        base.class_eval do 
+          my_klass.remote_attributes  = [:id, :name, :display_name]
+          my_klass.remote_permits     = [:id, :name, :display_name]
+          my_klass.remote_requires    = [:name, :display_name]
+        end
+      end
+    end
+
+  end
+end
