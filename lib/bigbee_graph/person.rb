4e4ffae8d7e3cc757ed8c2ccf4340f11a53ec741
joseph-bigbee <joseph@bigbeelabs.com>
2017-02-15 15:36:54 -0800

    adding acts_as_administering organizations and programs

--- lib/bigbee_graph/person.rb
+++ lib/bigbee_graph/person.rb
@@ -2,5 +2,7 @@ class BigbeeGraph::Person < BigbeeGraph::Base
   my_klass.remote_attributes  = [:id, :first_name, :last_name, :date_of_birth, :created_at, :updated_at, :sex_id, :ethnicity_id, :errors]
   my_klass.remote_permits     = [:first_name, :last_name]
   my_klass.remote_requires    = [:id, :first_name, :last_name, :date_of_birth, :sex_id, :ethnicity_id]
+  acts_as_administrating :organizations,  class_name: "BigbeeGraph::Organization",  remote: true
+  acts_as_administrating :programs,       class_name: "BigbeeGraph::Program",       remote: true
   
 end
