1f3c73269c582eebf06900fa3a60cb7a024d374f
joseph-bigbee <joseph@bigbeelabs.com>
2017-06-06 19:33:34 -0700

    removing BigbeeGraph classes and reverting to modules

--- lib/bigbee_graph/email_address.rb
+++
@@ -1,5 +0,0 @@
-class BigbeeGraph::EmailAddress < BigbeeGraph::Base
-  my_klass.remote_attributes  = [:id, :address, :created_at, :updated_at, :errors]
-  my_klass.remote_requires    = [:address]
-  my_klass.remote_permits     = [:address]
-end
--- lib/bigbee_graph/program.rb
+++
@@ -1,9 +0,0 @@
-require_dependency 'bigbee_graph/person'
-
-class BigbeeGraph::Program < BigbeeGraph::Base
-  my_klass.remote_attributes  = [:id, :name, :description, :system_name, :errors]
-  my_klass.remote_permits     = [:id, :name, :description, :system_name, :slug]
-  my_klass.remote_requires    = [:name, :description, :system_name]
-
-  acts_as_having :enrollees, class_name: "BigbeeGraph::Person", remote: true
-end
