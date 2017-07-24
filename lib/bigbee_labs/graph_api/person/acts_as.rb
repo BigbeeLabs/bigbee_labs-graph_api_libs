9d09a48a7ca4b23d867a0284233a3a51bcb172b8
joseph-bigbee <joseph@bigbeelabs.com>
2017-07-24 14:36:28 -0700

    acts_as_sharing_relationship_invitations_with

--- lib/bigbee_labs/graph_api/person/acts_as.rb
+++ lib/bigbee_labs/graph_api/person/acts_as.rb
@@ -9,6 +9,7 @@ module BigbeeLabs::GraphApi::Person::ActsAs
         base.acts_as_having :phone_numbers, class_name: "BigbeeGraph::PhoneNumber", remote: true
         base.acts_as_having :physical_addresses, class_name: "BigbeeGraph::PhysicalAddress", remote: true
         base.acts_as_relating_to :people, class_name: "BigbeeGraph::Person", remote: true
+        base.acts_as_sharing_relationship_invitations_with :people, class_name: 'BigbeeGraph::Person', remote: true
       end
     end
 
