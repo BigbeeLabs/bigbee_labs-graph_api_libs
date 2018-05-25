4c103a6ebaa8ccbb22b0d8bdd963539e1bbf1099
joseph-bigbee <joseph@bigbeelabs.com>
2018-05-24 19:39:30 -0700

    person acts_as

--- lib/bigbee_labs/graph_api/person/acts_as.rb
+++ lib/bigbee_labs/graph_api/person/acts_as.rb
@@ -5,6 +5,7 @@ module BigbeeLabs::GraphApi::Person::ActsAs
     def self.included(base)
       base.class_eval do 
         base.acts_as_administrating :organizations, class_name: "BigbeeGraph::Organization", remote: true
+        base.acts_as_administrating :groups, class_name: "BigbeeGraph::Group", remote: true
         base.acts_as_having :email_addresses, class_name: "BigbeeGraph::EmailAddress", remote: true
         base.acts_as_having :phone_numbers, class_name: "BigbeeGraph::PhoneNumber", remote: true
         base.acts_as_having :physical_addresses, class_name: "BigbeeGraph::PhysicalAddress", remote: true
