e95a3ec1ca1aa2022763c1d320a68cb0f33db819
joseph-bigbee <joseph@bigbeelabs.com>
2019-01-23 08:26:46 -0800

    verify email_address

--- lib/bigbee_labs/graph_api/email_address.rb
+++ lib/bigbee_labs/graph_api/email_address.rb
@@ -4,9 +4,9 @@ module BigbeeLabs
 
       def self.included(base)
         base.class_eval do  
-          my_klass.remote_attributes  += [:id, :address, :created_at, :updated_at, :errors, :is_primary, :label, :credential, :label]
+          my_klass.remote_attributes  += [:id, :address, :created_at, :updated_at, :errors, :is_primary, :label, :credential, :label, :validation_status]
           my_klass.remote_requires    += [:address]
-          my_klass.remote_permits     += [:address, :label]
+          my_klass.remote_permits     += [:address, :label, :validation_status]
         end
       end
 
