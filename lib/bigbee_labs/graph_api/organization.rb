59c6b00bb9d9d25c795a124850ee5ea62a7c9937
Joseph Villa <joseph@bigbeelabs.com>
2019-11-05 13:34:43 -0800

    adding acts_as_administrated_by :people to BigbeeGraph::Organization

--- lib/bigbee_labs/graph_api/organization.rb
+++ lib/bigbee_labs/graph_api/organization.rb
@@ -8,8 +8,10 @@ module BigbeeLabs
           my_klass.remote_requires    += [:full_name, :description, :code]
           my_klass.remote_permits     += [:id, :short_name, :full_name, :ein, :description, :code, :slug]
           extend ActsAsRelatingTo
+
           acts_as_having :programs, class_name: 'BigbeeGraph::Program', remote: true
           acts_as_having :groups, class_name: 'BigbeeGraph::Group', remote: true, api_version: '1'
+          acts_as_administrated_by :people, class_name: 'BigbeeGraph::Person', remote: true
 
           api_version 'v1'
           
