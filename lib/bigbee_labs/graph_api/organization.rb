b08c7e444688aedd965b949cc6917cebba1e94b0
Joseph Villa <joseph@bigbeelabs.com>
2021-06-29 15:25:29 -0700

    Commenting out acts_as_having :contacts for now

--- lib/bigbee_labs/graph_api/organization.rb
+++ lib/bigbee_labs/graph_api/organization.rb
@@ -13,7 +13,9 @@ module BigbeeLabs
           # TODO Remove next line? removed the 'api_version' argument. seeing if it works.
           #acts_as_having :groups, class_name: 'BigbeeGraph::Group', remote: true, api_version: '1'
           acts_as_having :groups, class_name: 'BigbeeGraph::Group', remote: true
-          acts_as_having :contacts, class_name: 'BigbeeGraph::Contact', remote: true
+          # TODO remove or remediate the following line. The idea of a 'Contact' may be a dud. It may be 
+          # People or Organizations instead
+          #acts_as_having :contacts, class_name: 'BigbeeGraph::Contact', remote: true
           
           acts_as_administrated_by :people, class_name: 'BigbeeGraph::Person', remote: true
 
